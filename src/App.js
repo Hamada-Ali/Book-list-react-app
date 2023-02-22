d4997b35685cd4b2f715d4589e3bd106a67d2499
Hamada Ali Elsayad <35568087+Hamada-Ali@users.noreply.github.com>
2023-02-23 01:43:25 +0200

    local api phase

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
 import { useState } from "react"
 import BookCreate from './components/BookCreate'
 import BookList from "./components/BookList"
+import axios from "axios";
+import { useEffect } from "react";
 
 const App  = () => {
+
     const [init, update] = useState([])
 
-    const deletedBooks = (id) => {
+    const deletedBooks = async (id) => {
+        const deleteRequest = await axios.delete(`http://localhost:3001/books/${id}`)
         const deleteFilter = init.filter((book) => {
             return book.id !== id
         })
         update(deleteFilter)
     }
 
-    const create = (title) => {
-        const newBook = [...init, {title, id:Math.floor(Math.random() * 1000)}]
+
+    const create = async (title) => {
+        const response = await axios.post("http://localhost:3001/books", {
+            title
+        })
+        const newBook = [...init, response.data]
         update(newBook)
-        console.log(init.length, newBook)
     }
 
-    const editTitle = (id, newTitle) => {
-        const allBooks = init.map((book) => {
-            if(book.id === id) {
-                return {...book, title:newTitle}
-            }
-            return book
+    const editTitle = async (id, newTitle) => {
+        const updateTitle = await axios.put((`http://localhost:3001/books/${id}`), {
+            title: newTitle
         })
-        update(allBooks)
+        const fullData = init.map((book) => {
+            if(id === book.id) {
+                return {...book, ...updateTitle.data}
+            }
+            return book;
+        })
+        update(fullData)
+
     }
+
+    const fetch = async () => {
+            const theData = await axios.get("http://localhost:3001/books")
+             update(theData.data)
+    }
+
+    useEffect(() => {
+        fetch()
+    }, [])
 
     return (
         <div className="app">
